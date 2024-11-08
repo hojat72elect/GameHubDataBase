277cd8ed09aa641ad3f8d2ff7cbef4bf4a1aa29b
hojat72elect <hojat72elect@gmail.com>
2024-11-08 02:21:39 -0500

    Got the names of some games from RAWG website.

---
+++ src/main.py
@@ -0,0 +1,19 @@
+def RawgScraper1():
+    import requests
+    from bs4 import BeautifulSoup
+
+    response = requests.get("https://rawg.io/?filters=%7B%22ordering%22%3A%5B%22-added%22%5D%7D")
+    html_content = response.content
+    soup = BeautifulSoup(html_content, 'html.parser')
+    links = soup.find_all('a', class_='game-card-medium__info__name')
+
+    with open('top_games_rawg_big_version.txt', 'a') as f:
+        for link in links:
+            game_link = link.get("href")
+            print(game_link)
+            f.write(game_link + '\n')
+
+
+if __name__ == '__main__':
+    RawgScraper1()
+    RawgScraper1()
